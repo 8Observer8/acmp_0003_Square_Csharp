c7f2a77fe9ecd648dcdd3d0e7a20b21d1e357ac0
Ivan <8observer8@gmail.com>
2013-05-12 19:55:31 +0400

    There are stub, tests and documentation.

---
+++ UnitTestCsharp/UnitTest1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using amcp_0003_Square_Csharp;
+
+namespace UnitTestCsharp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSquare_input_is_5()
+        {
+            Square s = new Square();
+            int number = 5;
+            int result = Square.square(number);
+            int expected = 25;
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestSquare_input_is_3()
+        {
+            Square s = new Square();
+            int number = 3;
+            int result = Square.square(number);
+            int expected = 9;
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestSquare_input_is_1()
+        {
+            Square s = new Square();
+            int number = 1;
+            int result = Square.square(number);
+            int expected = 1;
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestSquare_input_is_zero()
+        {
+            Square s = new Square();
+            int number = 0;
+            int result = Square.square(number);
+            int expected = 0;
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestSquare_input_is_minus_1()
+        {
+            Square s = new Square();
+            int number = -1;
+            int result = Square.square(number);
+            int expected = 1;
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestSquare_input_is_minus_5()
+        {
+            Square s = new Square();
+            int number = -5;
+            int result = Square.square(number);
+            int expected = 25;
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ amcp_0003_Square_Csharp/Square.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace amcp_0003_Square_Csharp
+{
+    /**
+     * Implementation of a class Square
+     */
+    public class Square
+    {
+        /**
+         * Raises a number to a square
+         * 
+         * @param number The number
+         * @return The square
+         */
+        public static int square(int number)
+        {
+            int result = 0;
+
+
+
+            return result;
+        }
+    }
+}
